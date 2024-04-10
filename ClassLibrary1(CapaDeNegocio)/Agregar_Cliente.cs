5616697041dba5e8e334f65c20367d70e3b3ddbe
ChuchulloFloresJohnRuben <201123187j@utea.edu.pe>
2024-04-10 11:08:27 -0500

    se agrega clase agregarEmpleado en la capa de negocio

---
+++ ClassLibrary1(CapaDeNegocio)/Agregar_Cliente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CapaEntidades; 
+using CapaDatos;    
+
+namespace CapaNegocio
+{
+    public class AgregarEmpleado
+    {
+        
+        public static void Agregar(string nombre, string apellidos, double sueldoBruto, string categoria)
+        {
+            
+            Empleado empleado = new Empleado(nombre, apellidos, sueldoBruto, categoria);
+
+            
+            GestorEmpleados.CalcularSueldoNeto(empleado);
+
+            
+            if (DatosEmpleado.Insertar(empleado))
+            {
+                Console.WriteLine("Empleado agregado exitosamente.");
+            }
+            else
+            {
+                Console.WriteLine("Error al agregar empleado. Por favor, inténtelo de nuevo.");
+            }
+        }
+    }
+}
